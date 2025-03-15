c2682aff9265c096ec9fa4117df7804ec9e9a5dc
ShAm1-228 <ykoty14@gmail.com>
2025-03-16 02:55:55 +0700

    inf jump.lua

--- speedhack.lua
+++ speedhack.lua
@@ -1,25 +1,21 @@
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 
--- Устанавливаем базовую скорость
 local speedBoost = 100
 local normalWalkSpeed = character.Humanoid.WalkSpeed
 
--- Функция для включения спидхаука
 local function enableSpeedHack()
 	character.Humanoid.WalkSpeed = normalWalkSpeed + speedBoost
 end
 
--- Функция для отключения спидхаука
 local function disableSpeedHack()
 	character.Humanoid.WalkSpeed = normalWalkSpeed
 end
 
--- Привязываем функции к событиям, например, к клавише
 local UserInputService = game:GetService("UserInputService")
 
 UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
-	if input.KeyCode == Enum.KeyCode.6 and not gameProcessedEvent then -- при нажатии E
+	if input.KeyCode == Enum.KeyCode.6 and not gameProcessedEvent then
 		enableSpeedHack()
 	end
 end)
